7c1e5677e46486284a41ef6383e34b948f031ef3
Yiwei Zhao <2294622497@qq.com>
2020-03-28 19:39:10 +0800

    【BUG FIX】wokingHourService没有加@service注解 导致项目运行失败

--- src/main/java/com/jcohy/scis/service/impl/WorkingHourServiceImpl.java
+++ src/main/java/com/jcohy/scis/service/impl/WorkingHourServiceImpl.java
@@ -6,9 +6,11 @@ import com.jcohy.scis.repository.WorkingHourRepository;
 import com.jcohy.scis.service.AchProjectService;
 import com.jcohy.scis.service.WorkingHourService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class WorkingHourServiceImpl implements WorkingHourService {
     @Autowired
     private WorkingHourRepository workinghourRepository;
